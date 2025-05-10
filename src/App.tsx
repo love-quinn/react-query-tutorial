9be7436916dcfbfcbe99a0d8d270912083280078
love-quinn <lucaslucal75@gmail.com>
2025-05-10 14:03:42 -0300

    feat: add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,16 @@ import { Button } from "./components/ui/button"
 import { Dialog, DialogTrigger } from "./components/ui/dialog"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table"
 import CreateProductDialog from "./components/create-product-dialog"
-import ProductFilters from "./components/product-filters"
-import { products as initialProducts} from "./data/product"
-import { useState } from "react"
+
 
 function App() {
-  const [filterId, setFilterId] = useState("")
-  const [filterName, setFilterName] = useState("")
 
-  const filteredProducts = initialProducts.filter((product) => {
-    return (
-      (filterId === "" || product.id.toString().includes(filterId)) &&
-      (filterName === "" || product.name.toLowerCase().includes(filterName.toLowerCase()))
-    )
-  })
 
   return (
     <div className="p-6 max-w-4xl mx-auto space-y-4">
       <h1 className="text-3xl font-bold">Produtos</h1>
 
       <div className="flex items-center justify-between">
-        {/* Product Filters Component */}
-        <ProductFilters 
-          filterId={filterId}
-          setFilterId={setFilterId}
-          filterName={filterName}
-          setFilterName={setFilterName}
-        />
-
         <Dialog>
           <DialogTrigger asChild>
             <Button>
@@ -54,7 +36,7 @@ function App() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {filteredProducts.map((product) => {
+            {/* {filteredProducts.map((product) => {
               return (
                 <TableRow key={product.id}>
                   <TableCell>{product.id}</TableCell>
@@ -62,7 +44,7 @@ function App() {
                   <TableCell>{product.price.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</TableCell>
                 </TableRow>
               )
-            })}
+            })} */}
           </TableBody>
         </Table>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
+import { queryClient } from './lib/react-query.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
